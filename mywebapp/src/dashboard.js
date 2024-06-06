b87271a0d455057716e107cec23ee269bff31bb1
MarkSuttirak <suttirak.ch@zaviago.com>
2024-06-06 15:12:34 +0700

    update css codes

--- mywebapp/src/dashboard.js
+++ mywebapp/src/dashboard.js
@@ -30,7 +30,7 @@ function Dashboard(props) {
     async function logoutHandler() {
         await signOut({redirect: false});
     }
-    
+
     return <>
         <div className={styles.dashboard}>
             <div className={`${isSidebarOpen ? 'bg-black/50 w-screen h-screen fixed top-0 left-0 lg:hidden z-[8]' : ''}`} onClick={() => setIsSidebarOpen(false)}></div>
@@ -40,6 +40,7 @@ function Dashboard(props) {
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-chevrons-left"><path d="m11 17-5-5 5-5"/><path d="m18 17-5-5 5-5"/></svg>
                         Close
                     </button>
+
                     <button className='button-normal w-full justify-start hover:bg-gray-100 items-center gap-2' onClick={() => window.open("/", {target: "_blank"})}>
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-app-window"><rect x="2" y="4" width="20" height="16" rx="2"/><path d="M10 4v4"/><path d="M2 8h20"/><path d="M6 4v4"/></svg>
                         View Site
