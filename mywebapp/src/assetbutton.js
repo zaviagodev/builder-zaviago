6e3857a63e8c8bb0f223ee0b0e9812906c31886d
MarkSuttirak <suttirak.ch@zaviago.com>
2024-06-06 13:32:24 +0700

    add mobile sidebar function

--- mywebapp/src/assetbutton.js
+++ mywebapp/src/assetbutton.js
@@ -20,7 +20,10 @@ function AssetButton() {
       }
 
     return <>
-        <button className='button-normal w-full justify-start hover:bg-gray-100' onClick={openModalHandler}>Assets</button>
+        <button className='button-normal w-full justify-start hover:bg-gray-100 items-center gap-2' onClick={openModalHandler}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-files"><path d="M20 7h-3a2 2 0 0 1-2-2V2"/><path d="M9 18a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h7l4 4v10a2 2 0 0 1-2 2Z"/><path d="M3 7.6v12.8A1.6 1.6 0 0 0 4.6 22h9.8"/></svg>
+            Assets
+        </button>
 
         <Transition appear show={isOpen} as={Fragment}>
             <Dialog
--- mywebapp/src/dashboard.js
+++ mywebapp/src/dashboard.js
@@ -10,6 +10,7 @@ import styles from '../styles/Dashboard.module.css';
 function Dashboard(props) {
 
     const [loadedData, setLoadedData] = useState();
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false)
 
     useEffect(()=>{
         getPages();
@@ -31,19 +32,37 @@ function Dashboard(props) {
     
     return <>
         <div className={styles.dashboard}>
-
-            <div className='flex flex-col items-start min-w-[256px] p-4 gap-2 border-r border-r-[#F4F4F4] h-screen'>
-                <AddPage onAdd={getPages} />
-                <AssetButton fileType={'all'}/>
-                <Site mainHost={props.mainHost} owner={props.owner} />
-                <Account />
-                <button className='button-normal w-full justify-start hover:bg-gray-100' onClick={logoutHandler}>Logout</button>
+            <div className={`${isSidebarOpen ? 'bg-black/50 w-screen h-screen fixed top-0 left-0 lg:hidden z-[8]' : ''}`} onClick={() => setIsSidebarOpen(false)}></div>
+            <div className={`fixed lg:static flex flex-col items-start min-w-[256px] border-r border-r-[#F4F4F4] h-screen bg-white z-[9] ${isSidebarOpen ? 'left-0' : '-left-[100%]'}`}>
+                <div className='p-4 border-b border-b-[#F4F4F4] gap-2 w-full'>
+                    <button className='p-0 mb-4 bg-white gap-2 items-center lg:hidden' onClick={() => setIsSidebarOpen(false)}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-chevrons-left"><path d="m11 17-5-5 5-5"/><path d="m18 17-5-5 5-5"/></svg>
+                        Close
+                    </button>
+                    <button className='button-normal w-full justify-start hover:bg-gray-100 items-center gap-2' onClick={() => window.open("/", {target: "_blank"})}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-app-window"><rect x="2" y="4" width="20" height="16" rx="2"/><path d="M10 4v4"/><path d="M2 8h20"/><path d="M6 4v4"/></svg>
+                        View Site
+                    </button>
+                    <Site mainHost={props.mainHost} owner={props.owner} />
+                </div>
+                <div className='p-4 border-b border-b-[#F4F4F4] gap-2 w-full'>
+                    <AddPage onAdd={getPages} />
+                    <AssetButton fileType={'all'}/>
+                    <Account />
+                </div>
             </div>
 
-            <div className='p-12 w-full bg-front-gradient'>
+            <div className='absolute p-4 border-b border-b-[#F4F4F4] top-0 right-0 w-full lg:w-[calc(100%_-_256px)] bg-white z-[7]'>
+                <div className='flex justify-between lg:justify-end'>
+                    <button className='p-0 bg-white lg:hidden' onClick={() => setIsSidebarOpen(true)}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-chevrons-right"><path d="m6 17 5-5-5-5"/><path d="m13 17 5-5-5-5"/></svg>
+                    </button>
+                    <button className='p-0 bg-white' onClick={logoutHandler}>Logout</button>
+                </div>
+            </div>
 
+            <div className='px-5 py-8 w-full bg-front-gradient mt-[53px]'>
                 <h1>Dashboard</h1>
-
 
                 <div className="flex flex-wrap">
                     {loadedData?loadedData.pages.map(page=>{
