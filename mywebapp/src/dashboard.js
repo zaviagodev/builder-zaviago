c7336c531f51404f47682d7eb945ce27c768e3c0
MarkSuttirak <suttirak.ch@zaviago.com>
2024-06-06 17:59:27 +0700

    update css codes

--- mywebapp/src/dashboard.js
+++ mywebapp/src/dashboard.js
@@ -76,7 +76,10 @@ function Dashboard(props) {
                             <Menu.Items className={`${styles.moremenu} focus:outline-none !top-12`}>
                                 <Menu.Item>
                                     {({ active }) => (
-                                        <button className={`${active ? styles.active : ""}`} title={'Logout'} onClick={logoutHandler}>Logout</button>
+                                        <button className={`${active ? styles.active : ""}`} title={'Logout'} onClick={logoutHandler}>
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-log-out"><path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"/><polyline points="16 17 21 12 16 7"/><line x1="21" x2="9" y1="12" y2="12"/></svg>
+                                            Logout
+                                        </button>
                                     )}
                                 </Menu.Item>
                             </Menu.Items>
